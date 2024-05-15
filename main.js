2cf108562d2c06c2692ec17c284fa1bd08dc6874
Mikey Nichols <mnix@journeytocode.io>
2024-05-15 02:13:58 -0400

    creates a color-picker assis tant

---
+++ main.js
@@ -0,0 +1,51 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+ 
+async function main() { 
+  const assistant = await openai.beta.assistants.create({
+    name: "Color Scheme Generator",
+    instructions: "Take a color or mood and return a color scheme in JSON format that represents the input well. Provide some information about why each color was picked as well",
+    tools: [],
+    model: "gpt-4o"
+  });
+  const thread = await openai.beta.threads.create();
+const message = await openai.beta.threads.messages.create(
+    thread.id,
+    {
+      role: "user",
+      content: "fascinating"
+    }
+  );
+
+  // We use the stream SDK helper to create a run with
+// streaming. The SDK provides helpful event listeners to handle 
+// the streamed response.
+ 
+const run = openai.beta.threads.runs.stream(thread.id, {
+    assistant_id: assistant.id
+  })
+    .on('textCreated', (text) => process.stdout.write('\nassistant > '))
+    .on('textDelta', (textDelta, snapshot) => process.stdout.write(textDelta.value))
+    .on('toolCallCreated', (toolCall) => process.stdout.write(`\nassistant > ${toolCall.type}\n\n`))
+    .on('toolCallDelta', (toolCallDelta, snapshot) => {
+      if (toolCallDelta.type === 'code_interpreter') {
+        if (toolCallDelta.code_interpreter.input) {
+          process.stdout.write(toolCallDelta.code_interpreter.input);
+        }
+        if (toolCallDelta.code_interpreter.outputs) {
+          process.stdout.write("\noutput >\n");
+          toolCallDelta.code_interpreter.outputs.forEach(output => {
+            if (output.type === "logs") {
+              process.stdout.write(`\n${output.logs}\n`);
+            }
+          });
+        }
+      }
+    });
+}
+ 
+main();
+
